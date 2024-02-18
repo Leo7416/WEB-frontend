804a9df46d2dfdfc509b2d3545e7da8c4ccda064
Leo7416 <Adolfhiko@gmail.com>
2024-02-19 02:59:04 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: 'WaterMeter_ReactApp/',
+  base: '/WEB-frontend',
   plugins: [
     react(),
     VitePWA({ 
