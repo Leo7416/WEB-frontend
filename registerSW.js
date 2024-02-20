82daf5c99c18f5e847be55fae16693c7494f3418
Leo7416 <Adolfhiko@gmail.com>
2024-02-20 14:27:49 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/WEB-frontend/sw.js', { scope: '/WEB-frontend/' })})}
